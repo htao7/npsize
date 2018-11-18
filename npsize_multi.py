9f6eed3057eb5b99c4636071f628c611ba4a0fe6
taotao720 <44954302+taotao720@users.noreply.github.com>
2018-11-18 00:45:00 -0400

    Update npsize_multi.py

--- npsize_multi.py
+++ npsize_multi.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
+IMG_R = 4
+
 def nothing(x):
     pass
 
@@ -25,48 +27,53 @@ def calc_mag():
         if score >= score_max:
             score_max = score
             magi = sb
-    img_sb = img[2475:2485,1600:3296] #scalebar image
+    img_sb = img[2475:2485,1600:3296] # scale bar image
     ret,img_sb = cv2.threshold(img_sb,50,255,cv2.THRESH_BINARY_INV)
     _,sbcont,_ = cv2.findContours(img_sb,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     mag = float(magi)/(sbcont[0][0][0][0]-sbcont[10][0][0][0])
     return mag
 
-def findsphere():
+def FindSphere():
     pass
 
-def findcube():
+def FindCube():
     if w > 30 and h/w <1.2 and area*mag**2/(w*h)>0.88:
         cv2.drawContours(im, [box], 0, (0,255,0), 2)
-        global inset_center, inset_l
-        inset_center = (int(rect[0][1]),int(rect[0][0]))
-        inset_l = int(max(rect[1]))
         return 1
     else:
         return 0
 
-def findrod():
+def FindRod():
     if w > 20 and h/w >3 and area*mag**2/(w*h)>0.8:
         cv2.drawContours(im, [box], 0, (0,255,0), 2)
-        global inset_center, inset_l
-        inset_center = (int(rect[0][1]),int(rect[0][0]))
-        inset_l = int(max(rect[1]))
         return 1
     else:
         return 0
 
-def draw_inset():
-    r1 = max(inset_center[0]-inset_l,0)
-    r2 = max(inset_center[0]+inset_l,0)
-    c1 = max(inset_center[1]-inset_l,0)
-    c2 = max(inset_center[1]+inset_l,0)
-    im_inset = np.copy(im[r1:r2,c1:c2])
-    cv2.imshow('inset',im_inset)
-    
+def enlarge(event,x,y,flags,param):
+    global ix,iy,captureflag,x1,x2,y1,y2
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        captureflag = True
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if captureflag == True:
+            x1 = min(ix,x)
+            x2 = max(ix,x)
+            y1 = min(iy,y)
+            y2 = max(iy,y)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        captureflag = False
+
+        
 dirpath = os.getcwd()
 nptype = -1 #nanoparticle type
 nph = 0
 npw = 0
 npn = 0
+captureflag = False
 
 
 for foldername in os.listdir(dirpath):
@@ -82,7 +89,11 @@ for foldername in os.listdir(dirpath):
         if filename.endswith('.jpg') or filename.endswith('.tif'):
             img0 = cv2.imread(dirpath + '\\' + foldername + '\\' + filename)
             img = cv2.cvtColor(img0,cv2.COLOR_BGR2GRAY)
+            x1 = y1 = 0
+            x2 = y2 = 1
             r,c = img.shape
+            r2 = int(r/IMG_R)
+            c2 = int(c/IMG_R)
             mag = calc_mag()
             #img_blur = cv2.GaussianBlur(img,(5,5),0)
             img_blur = cv2.medianBlur(img[0:2450,0:3296],5)
@@ -90,6 +101,7 @@ for foldername in os.listdir(dirpath):
             #plt.show()
             cv2.namedWindow('dst')
             cv2.createTrackbar('Thresh','dst',50,100,nothing)
+            cv2.setMouseCallback('dst',enlarge)
             threshflag = -1
             while(True):
                 thresh = cv2.getTrackbarPos('Thresh','dst')
@@ -111,23 +123,25 @@ for foldername in os.listdir(dirpath):
                         h = max(rect[1])*mag
                         isnp = 0
                         if nptype == 0:
-                            isnp = findsphere()
+                            isnp = FindSphere()
                         elif nptype == 1:
-                            isnp = findcube()
+                            isnp = FindCube()
                         elif nptype == 2:
-                            isnp = findrod()
+                            isnp = FindRod()
                         if isnp == 1:
                             width += w
                             height += h
                             count += 1
                     if count > 0:
-                            print ('n='+str(count)+'\n')
-                    im_resize = cv2.resize(im,(800,600),interpolation = cv2.INTER_AREA)
-                    draw_inset()
-                    cv2.imshow('dst',im_resize)
+                            print ('n=' + str(count) + '\n')
                     threshflag = thresh
-                    #distflag = dist
-                k = cv2.waitKey(10) & 0xFF
+                im_resize = cv2.resize(im,(c2,r2),interpolation = cv2.INTER_AREA)
+                cv2.rectangle(im_resize,(x1,y1),(x2,y2),(0,0,255),1)
+                cv2.imshow('dst',im_resize)
+                if captureflag == False:
+                    im_inset = im[y1*IMG_R:y2*IMG_R,x1*IMG_R:x2*IMG_R]
+                    cv2.imshow('inset',im_inset)
+                k = cv2.waitKey(1) & 0xFF
                 if k == ord('n'):
                     npw += width
                     nph += height
@@ -136,14 +150,14 @@ for foldername in os.listdir(dirpath):
                         break
                     print ('Until now:')
                     if nptype == 0:
-                        print ('r='+str((npw+nph)/2/npn)+'\nnum='+str(npn)+'\n')
+                        print ('r=' + str((npw+nph)/2/npn) + '\nnum=' + str(npn) + '\n')
                     elif nptype == 1:
-                        print ('l='+str((npw+nph)/2/npn)+'\nnum='+str(npn)+'\n')
+                        print ('l=' + str((npw+nph)/2/npn) + '\nnum=' + str(npn) + '\n')
                     elif nptype == 2:
-                        print ('w='+str(npw/npn)+'\nh='+str(nph/npn)+'\nnum='+str(npn)+'\n')
+                        print ('w=' + str(npw/npn) + '\nh=' + str(nph/npn) + '\nnum=' + str(npn) + '\n')
                     cv2.destroyAllWindows()
                     break
-                if k == 27:
+                elif k == 27:
                     cv2.destroyAllWindows()
                     exit()
 cv2.waitKey(0)
